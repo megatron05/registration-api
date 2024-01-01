66705e0dd2ec21d93ca02e68fd41800f96594dc0
Dheeraj Kumar Kondapalli <dheerajkumar200005@gmail.com>
2024-01-01 17:26:34 -0600

    registration code changes

--- registration-api/src/main/java/com/ecom/registrationapi/Model/User.java
+++ registration-api/src/main/java/com/ecom/registrationapi/Model/User.java
@@ -23,18 +23,51 @@ public class User {
     private String password;
     private int phoneNumber;
 
+    public Integer getUserId() {
+        return userId;
+    }
 
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public String getPassword() {
         return password;
     }
 
-    public void setFirstName(String firstName) {
+    public void setPassword(String password) {
+        this.password = password;
     }
 
-    public void setLastName(String lastName) {
+    public int getPhoneNumber() {
+        return phoneNumber;
     }
 
-    public void setPhoneNumber(Integer phoneNumber) {
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 }
